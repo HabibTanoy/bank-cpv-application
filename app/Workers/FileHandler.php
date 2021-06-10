b81811ed42d29c9939c818fc0236c4518976d542
tanjed <tanjed.15.6849@gmail.com>
2021-06-10 16:43:31 +0600

    Route Fixed

--- app/Workers/FileHandler.php
+++ app/Workers/FileHandler.php
@@ -7,14 +7,14 @@ class FileHandler{
 
     function __construct()
     {
-        $this->root = 'storage/app/';
+        $this->root = 'storage';
     }
     public function uploadFile($file,$file_name)
     {
         try{
             $file_name = $file_name.'.'.$file->getClientOriginalExtension();
             Storage::disk('public')->put($file_name,$file->getContent());
-            return public_path('storage').DIRECTORY_SEPARATOR.$file_name;
+            return $this->root.DIRECTORY_SEPARATOR.$file_name;
         }catch(\Exception $exception){
             dd($exception);
             throw new Exception($exception->getMessage());
